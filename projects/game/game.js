31b224a2db734d39bc8e931c6e8544372025e714
Michael Folb <michaelhfolb@gmail.com>
2026-03-09 08:37:55 -0400

    Test_1

--- projects/game/game.js
+++ projects/game/game.js
@@ -14,7 +14,7 @@ function townHall() {
     print("\nWhere do you want to go next? Say one of these choices:" +
         "\n\t Port restaurants \n\t Police station \n\t Kennedy Space Center \n\t Beach" );
 
-    
+   
     function processInput(input){
         if (input.toLowerCase() === "port restaurants") {
             portRestaurants();
@@ -42,6 +42,39 @@ function  portRestaurants() {
     }
     waitForInput(processInput);
 }
+
+   
+    function processInput(input){
+        if (input.toLowerCase() === "police station") {
+            policeStation();
+        } else {
+            stayHere();
+            waitThenCall(townHall);
+        }
+    }
+    waitForInput(processInput);
+}
+
+function  policeStation() {
+    clear();
+    print("\nYou are at the Police station!");
+    print("\nWhere do you want to go next? Say one of these choices:" +
+        "\n\t Town hall");
+    
+    function processInput(input){
+        if (input.toLowerCase() === "town hall") {
+            townHall();
+        } else {
+            stayHere();
+            waitThenCall(policeStation);
+        }
+    }
+    waitForInput(processInput);
+}
+
+/
+
+
 
 //finally, make sure you customize this to tell it what should happen at the
 //very start. For this simple example, any input will bring you
