925aedd574b4880aab334dc21efbfc06d069bf7a
Michael Folb <michaelhfolb@gmail.com>
2026-03-16 17:00:47 -0400

    I can not get varibles to work

--- projects/game/game.js
+++ projects/game/game.js
@@ -87,7 +87,13 @@ function  ksc() {
 
 function  school() {
     clear();
+	if (peacockTreat) {
     print("\nYou walk up to the school to see choas. Bright peacocks are chasing kids into the school, and a teacher yells at you from the other side of the fence to go get some treats. When you ask where, she tells you the Cumberland farms. It's just down the road.");
+	} else if (peacockTreat) {
+	print("You walk up to the pretty birds, and start throwing out the seeds. They quickly follow, away from the school. You turn back to see everyone going home. Good job!")
+	print("Congrats, you saved the day!")
+	gameActive = false;
+	}
     print("\nWhere do you want to go next? Say one of these choices:" +
         "\n\t Town hall \n\t Cumberland Farms");
     
@@ -110,7 +116,7 @@ function  cf() {
 	peacockTreat = True;
     print("\nWhere do you want to go next? Say one of these choices:" +
         "\n\t Town hall \n\t School");
-    
+
     function processInput(input){
         if (input.toLowerCase() === "town hall") {
             townHall();
